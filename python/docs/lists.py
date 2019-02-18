3c6810bd458596b361cd6601a49a116ddedda192
merwynl <maawin.rimu@gmail.com>
2019-02-18 23:15:45 +1100

    check in lists & madlibs py

--- python/docs/lists.py
+++ python/docs/lists.py
@@ -3,3 +3,26 @@
 #############
 # Lists
 #############
+
+# Index in Python starts at 0
+singers = ['れをる', 'あいみょん', 'えなこ', 'みゆな']
+
+# Lists can take in more than one data type
+numbers = ['れをる', 2, True, 1.256]
+
+# Prints the entire list values
+print(singers)
+
+# Prints the index value from the list
+print(singers[0])
+
+# Prints the values from the list starting with the given index
+print(singers[2:])
+
+# Prints from a range of index values in a given list
+print(singers[1:3])
+
+# Replaces the index value with another value
+singers[1] = '鈴木里奈'
+print(singers[1])
+
