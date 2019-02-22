ad1af83370bf1a6c6313767e4e1f2b84918f5f7a
merwynl <maawin.rimu@gmail.com>
2019-02-22 17:24:36 +1100

    add guessing game & while loop docs. expand dictionary doc

---
+++ python/docs/while.py
@@ -0,0 +1,17 @@
+#################
+# While
+#################
+
+# Be on the lookout for infinite loops, they can crash a programs
+i = 1
+
+# While i is less than the given variable, do the function
+num = 100
+while i <= num:
+    print(i)
+    i += 1
+
+    # alternate method of incrementing
+    # i = i +1
+
+print('Done!')
